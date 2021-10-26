58d01fc49f5379925f136ab6fdbf2b37d4b35490
aibeam <aibeam1025@gmail.com>
2021-10-26 15:03:13 +0900

    Initial commit

---
+++ antddun/src/main/java/com/ds/antddun/AntddunApplication.java
@@ -0,0 +1,13 @@
+package com.ds.antddun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntddunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntddunApplication.class, args);
+	}
+
+}
